154ebf16f7fde58afe05b01f022b0dee51bfcc84
poundtheerapat <theerapat.p@ku.th>
2017-03-09 15:48:05 +0700

    resent

--- challenges-solved/5910406175.c
+++ challenges-solved/5910406175.c
@@ -1,53 +1,53 @@
 //c0004
 #include <stdio.h>
 float f,s,S,result ; // variable now is global 
-float a () // if still 'f' it will be redeclare 'a' is 'f' function
+float focus () // if still 'f' it will be redeclare 'a' is 'f' function
 {
 
     puts("Input s = ");
-	scanf("%f",&s);
-	puts("Input S = ");
-	scanf("%f",&S);
-	f = 1/(1/s+1/S);
-	return f;
+    scanf("%f",&s);
+    puts("Input S = ");
+    scanf("%f",&S);
+    f = 1/(1/s+1/S);
+    return f;
 }
-float b () // if still 's' it will be redeclare 'b' is 's' function
+float obj_distance () // if still 's' it will be redeclare 'b' is 's' function
 {
-	puts("Input f = ");
-	scanf("%f",&f);
-	puts("Input S = ");
-	scanf("%f",&S);
-	s=1.0/((S-f)/(f*S));
-	return s;
+    puts("Input f = ");
+    scanf("%f",&f);
+    puts("Input S = ");
+    scanf("%f",&S);
+    s=1.0/((S-f)/(f*S));
+    return s;
     
 }
-float c () // if still 'S' it will be redeclare 'c' is 'f' function
+float pic_distance () // if still 'S' it will be redeclare 'c' is 'f' function
 {
-	puts("Input f = ");
-	scanf("%f",&f);
-	puts("Input s = ");
-	scanf("%f",&s);
-	S = 1.0/((s-f)/(f*s));
+    puts("Input f = ");
+    scanf("%f",&f);
+    puts("Input s = ");
+    scanf("%f",&s);
+    S = 1.0/((s-f)/(f*s));
     return S;
 }
 void main ()
 {
-	char input ;
+    char input ;
 
-	printf("What do you want to find?\n");
-	printf("Wanna find? : \n");
-	scanf("%c", &input);
-	if(input == 'f')
-	{
-		result = a();
-	}
-	else if (input == 's')
-	{
-        result = b();
-	}
-	else if (input == 'S')
-	{
-       result = c();
-	}
-	printf("%c = %.1f",input,result);
+    printf("What do you want to find?\n");
+    printf("Wanna find? : \n");
+    scanf("%c", &input);
+    if(input == 'f')
+    {
+        result = focus();
+    }
+    else if (input == 's')
+    {
+        result = obj_distance();
+    }
+    else if (input == 'S')
+    {
+       result = pic_distance();
+    }
+    printf("%c = %.1f",input,result);
 }
