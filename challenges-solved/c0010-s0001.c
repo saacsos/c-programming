50447c390b0a8955b0f7428447a7efe1f7b6e8b3
Saac <saacsos@gmail.com>
2017-03-12 21:43:30 +0700

    waiting for approval

--- challenges-solved/c0010-s0001.c
+++ challenges-solved/c0010-s0001.c
@@ -1,3 +1,7 @@
+// code c0010
+// by @Briingz
+// status: waiting for approval
+
 #include <stdio.h>
 int main()
 {
@@ -12,11 +16,11 @@ int main()
 	if ((month<=0 || month>12) || (year<1900 || year>2020))
 		printf("\nERROR");
 	else
-	{	
+	{
 		printf("\nExpense :");
 		do{
 			printf("\n>> Day : ");scanf("%d",&day);
-			
+
 			if (day<0 || day>30){
 				printf("***** End *****\n\n");break;}
 
@@ -26,9 +30,9 @@ int main()
 
 		}while (day>0 && day<31);
 
-		int a,daymin,daymax,k=0; 
+		int a,daymin,daymax,k=0;
 		float min=5000000,max=0,total=0,av ;
-		
+
 		for(a=0;a<=30;a++){
 			if(allday[a]>0)
 			{
@@ -52,6 +56,6 @@ int main()
 			printf("Min expense : %.2f Baht. %02d/%02d/%4d\n",min,daymin,month,year);
 			printf("Avg expense : %.2f Baht.\n",av);
 		}
-	}	
+	}
 	return 0 ;
 }
