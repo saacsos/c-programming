0c7880a146f217ff3639a77b4449a55ee826ce68
soizensun <kitpavin@gmail.com>
2017-03-12 22:28:40 -1200

    ส่ง c0020

---
+++ challenges-solved/c0020.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main(){
+	char c;
+	float v, s, t;
+printf("[v]:Speed [s]:Distance [t]:Time\n");
+printf("Input Command: ");
+scanf("%c",&c);
+
+	if (c == 'v' || c == 'V'){
+		printf("Input a Distance: "); scanf("%f",&s);
+		printf("Input a time: "); scanf("%f",&t);
+		if ((s <= 0) || (t <= 0)){
+			printf("Invalid input.");
+		}
+		else {
+			v = s/t;
+			printf("Total Speed: %.2f m/s.",v);
+		}
+	}
+	else if (c == 's' || c == 'S'){  
+		printf("Input a Speed: "); scanf("%f",&v);
+		printf("Input a time: "); scanf("%f",&t);
+		if ((v <= 0) || (t <= 0)){
+			printf("Invalid input.");
+		}
+		else {
+			s = v*t;
+			printf("Total Distance: %.2f m.",s);
+		}	
+	}
+	else if (c == 't' || c == 'T'){
+		printf("Input a Distance: "); scanf("%f",&s);
+		printf("Input a Speed: "); scanf("%f",&v);
+		if ((s <= 0) || (v <= 0)){
+			printf("Invalid input.");
+		}
+		else {
+			t = s/v;
+			printf("Total time: %.2f sec.",t);
+		}	
+	}
+	else{
+		printf("Invalid command.\n");
+	}		
+
+
+
+}
