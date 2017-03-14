88ab1f5a9095fdf5b9c7e390bce67e04844aea7e
Saac <saacsos@gmail.com>
2017-03-14 09:28:23 +0700

    Using function stdtod

--- sample-code/08-characters-and-strings/fig08_06.c
+++ sample-code/08-characters-and-strings/fig08_06.c
@@ -15,4 +15,16 @@ int main(void)
 
     printf("The string \"%s\" is converted to the \n", string);
     printf("double value %.2f and the string \"%s\"\n", d, stringPtr);
+
+    puts("");
+    // unable to convert any portion of its first argument to double
+    const char *string2 = "There are 100 students in this class"; // initialize string
+    char *string2Ptr; // create char pointer
+
+    double d2 = strtod(string2, &string2Ptr);
+    // first of string is not able to convert to double,
+    // function strtod returns 0
+
+    printf("The string \"%s\" is converted to the \n", string2);
+    printf("double value %.2f and the string \"%s\"\n", d2, string2Ptr);
 }
