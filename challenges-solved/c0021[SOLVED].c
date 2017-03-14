f2e203d4d391b9dbac98f9c464661cda2c0b8e9c
thititon <thitiwat.te@ku.th>
2017-03-14 17:45:24 +0700

    Create c0021[SOLVED].c

---
+++ challenges-solved/c0021[SOLVED].c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+  int flr,all,brk=0,i=0,space;
+  printf("-----------------------------------------\nHow many floors do you want to build?:");
+  scanf("%d",&flr);
+  printf("-----------------------------------------\n");
+  for(i=0;i<flr;i++){
+    for(space=flr-i+1;space!=0;space--)
+      printf(" ");
+    printf("=");
+    
+    all+=1;
+    for(brk=i;brk!=0;brk--){
+      printf("==");
+      all+=2;
+    }
+    printf("\n");
+  }
+  printf("Number of bricks required: %d",all);
+  
+  //1025  c0021
+  
+}
