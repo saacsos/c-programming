eba8e43f6ed94768535d795f28f0bdeee9c8334c
Saac <saacsos@gmail.com>
2017-03-14 10:31:30 +0700

    Using functions strcat and strncat

---
+++ sample-code/08-characters-and-strings/fig08_16.c
@@ -0,0 +1,26 @@
+/*
+ * Using functions strcat and strncat
+ * To compile:
+ * $ gcc fig08_16.c -o fig08_16
+ */
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char s1[20] = "Happy "; // initialize char array s1
+    char s2[] = "New Year "; // initialize char array s2
+    char s3[40] = ""; // initialize char array s3 to empty string
+
+    printf("s1: %s\ns2: $s\n", s1, s2);
+
+    // concatenate s2 to s1
+    printf("strcat(s1, s2) = %s\n", strcat(s1, s2));
+
+    // concatenate first 6 characters of s1 to s3.
+    // place '\0' after last character
+    printf("strncat(s3, s1, 6) = %s\n", strncat(s3, s1, 6));
+
+    // concatenate s1 to s3
+    printf("strcat(s3, s1) = %s\n", strcat(s3, s1));
+}
