164463f5c3d9eb346f1e27acfd4568a29519f0ce
Saac <saacsos@gmail.com>
2017-04-21 22:08:55 +0700

    Date Class test program

--- sample-code/18-operator-overloading/fig18_06/fig18_08.cpp
+++ sample-code/18-operator-overloading/fig18_06/fig18_08.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main()
 {
+
     Date d1(12, 27, 2010); // December 27, 2010
     Date d2; // defaults to January 1, 1990
 
@@ -16,9 +17,11 @@ int main()
 
     cout << "d1 += 7 is " << (d1 += 7) << "\n\n";
 
+
     d2.setDate(2, 28, 2008);
     cout << "  d2 is " << d2 << endl;
     cout << "++d2 is " << ++d2 << " (leap year allows 29th)\n\n";
+
 
     Date d3(7, 13, 2010);
     cout << "Testing the prefix increment operator:" << endl
@@ -30,4 +33,5 @@ int main()
          << "  d3 is " << d3 << endl;
     cout << "d3++ is " << d3++ << endl;
     cout << "  d3 is " << d3 << endl;
+
 } // end main
